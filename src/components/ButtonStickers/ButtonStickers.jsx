f864649d9ce062ac71d2879c9df98fcb47f95fb4
Pam <pamellabviana@gmail.com>
2022-01-31 16:04:28 -0500

    added stickers

--- src/components/ButtonStickers/ButtonStickers.jsx
+++ src/components/ButtonStickers/ButtonStickers.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import './button-stickers.styles.css'
+import React from "react";
+import "./button-stickers.styles.css";
+import appConfig from "../../config.json";
 
-function ButtonStickers() {
-    return(
-        <button id="sticker-button">😋</button>
-    )
+function ButtonStickers(props) {
+  const [isOpen, setOpenState] = React.useState("");
+  return (
+    <>
+      <button id="sticker-button" onClick={() => setOpenState(!isOpen)}>
+        😋
+      </button>
+      {isOpen && (
+        <div
+          className="stickers-box-container"
+          onClick={() => setOpenState(false)}
+        >
+          <p id="title-stickers-box"> Stickers </p>
+          <ul className="stickers-list-container">
+            {appConfig.stickers.map((sticker) => (
+              <li
+                id="sticker-li"
+                key={sticker}
+                onClick={() => {
+                  if (Boolean(props.onStickerClick)) {
+                    props.onStickerClick(sticker);
+                  }
+                }}
+              >
+                <img id="sticker-img" alt="sticker" src={sticker} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default ButtonStickers;
